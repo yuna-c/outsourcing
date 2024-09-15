5b0590c198740af11eec805232c69afe80a487fa
songhyein <cha123hein@naver.com>
2024-09-15 18:50:30 +0900

    add:detail코드

---
+++ src/components/pages/Detail.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const API_URL = '';
+
+const fetchData = async () => {};
+
+const Detail = () => {
+  return (
+    <div>
+      <h2>약국이름</h2>
+      <p>주소 : </p>
+      <p>전화번호 : </p>
+      <p>영업시간 : </p>
+      <p>주말영업여부 : </p>
+    </div>
+  );
+};
+
+export default Detail;
