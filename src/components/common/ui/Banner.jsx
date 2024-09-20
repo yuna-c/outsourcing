02bfdee174d9af819ba0cfa8f1796b3ab2900767
지영 <rooftop7788@naver.com>
2024-09-20 20:08:29 +0900

    style: main 배너 이미지 수정

--- src/components/common/ui/Banner.jsx
+++ src/components/common/ui/Banner.jsx
@@ -5,8 +5,7 @@ import mainIcon from '/src/assets/images/main_icon.png';
 import './../../../assets/styles/mainPage.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const mainBanner =
-  'https://images.unsplash.com/photo-1612882515317-206847d253ff?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+import mainBanner from '/src/assets/images/pills.jpg';
 
 const Banner = ({ data }) => {
   const [query, setQuery] = useState('');
@@ -20,9 +19,8 @@ const Banner = ({ data }) => {
     }
   };
   return (
-    <div className="main_banner " style={{ backgroundImage: `url(${mainBanner})` }}>
+    <div className="main_banner " style={{ backgroundImage: `url(${mainBanner})`, backgroundPosition: 'top left' }}>
       <img src={mainIcon} alt="banner_icon" />
-      {/* <img src={mainDoctor} alt="doctor" className="" /> */}
 
       <div className="main_text_area ">
         <h3>약국찾기</h3>
