4c43d6047b942309162daac7ef1d4f25910fe82b
지영 <rooftop7788@naver.com>
2024-09-21 15:35:25 +0900

    style: select box 스타일 수정

--- src/components/common/ui/main/PharmaciesSection.jsx
+++ src/components/common/ui/main/PharmaciesSection.jsx
@@ -9,7 +9,12 @@ const PharmaciesSection = ({ title, pharmacies, REGIONS, tag }) => {
     <div className="current_pharmacies">
       <div className="pharmacy_selector_container">
         <h3 className="pharmacy_selector_title text-2xl">{title}</h3>
-        <SelectBox REGIONS={REGIONS} selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion} />
+        <SelectBox
+          REGIONS={REGIONS}
+          selectedRegion={selectedRegion}
+          setSelectedRegion={setSelectedRegion}
+          pharmacies={pharmacies}
+        />
       </div>
       <SliderResponse
         pharmacies={selectedRegion ? pharmacies.filter((item) => item.region.includes(selectedRegion)) : pharmacies}
--- src/components/common/ui/main/SelectBox.jsx
+++ src/components/common/ui/main/SelectBox.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './../../../../assets/styles/mainPage.css';
-const SelectBox = ({ REGIONS, selectedRegion, setSelectedRegion }) => {
+const SelectBox = ({ REGIONS, selectedRegion, setSelectedRegion, pharmacies }) => {
+  console.log(pharmacies);
   return (
     <div className="select_container">
       <select value={selectedRegion} onChange={(e) => setSelectedRegion(e.target.value)} className="border ">
-        <option value="지역선택">지역선택</option>
+        <option value={pharmacies}>지역선택</option>
         {REGIONS.map((region) => (
           <option key={region} value={region}>
             {region}
