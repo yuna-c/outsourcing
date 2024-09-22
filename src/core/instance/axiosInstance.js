247d38b297b6a7845326fd4e06cb82af10d7b1ec
yuna <yuna744@gmail.com>
2024-09-23 06:28:11 +0900

    🐛 feat : 로그인 경로 추가

--- src/core/instance/axiosInstance.js
+++ src/core/instance/axiosInstance.js
@@ -5,6 +5,7 @@ export const auth = axios.create({
 });
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000'
-  //  import.meta.env.MODE === 'development' ? 'http://localhost:5000' : import.meta.env.VITE_API_BASE_URL
+  baseURL: import.meta.env.VITE_API_BASE_URL
+  // baseURL: 'http://localhost:5000'
+  // import.meta.env.MODE === 'development' ? 'http://localhost:5000' : import.meta.env.VITE_API_BASE_URL
 });
